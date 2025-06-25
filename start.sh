490a0831e284fcc98916dbd0003345fd517d5855
Santiago Enamorado <sanstats03@gmail.com>
2025-06-24 19:59:41 -0500

    Corrijo start

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-echo "Ejecutando script R..."
-Rscript descargar_datos.R
-
-echo "Iniciando servidor HTTP en puerto 8080..."
-python3 -m http.server 8080
+echo "Ejecutando servidor en Render..."
+python app.py

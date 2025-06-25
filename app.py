d74c504b2d9455da16b999174681383ba6fc0eb4
Santiago Enamorado <sanstats03@gmail.com>
2025-06-24 21:46:57 -0500

    Fix: render escucha puerto para flask

--- app.py
+++ app.py
@@ -18,3 +18,7 @@ def index():
 @app.route('/<path:filename>')
 def serve_file(filename):
     return send_from_directory(BASE_DIR, filename)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
